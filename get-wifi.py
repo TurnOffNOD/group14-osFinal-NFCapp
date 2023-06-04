cb2c1789ff59e6bbe0f3346e9f7643c265f3ba0a
Tong Xian <tongxian22@mails.ucas.ac.cn>
2023-06-04 23:05:27 +0800

    add some func, add argument to not alway wake up.

--- get-wifi.py
+++ get-wifi.py
@@ -2,6 +2,7 @@
 
 import nfcheader
 import serial
+import argparse
 
 comm_method = "UART"
 comm_dev    = "/dev/ttyS5"
@@ -12,5 +13,13 @@ stopbits    = 1
 
 os_final_exp3_nfc = nfcheader.PN532_board(comm_method, comm_dev, baudrate, bytesize, pairity, stopbits)
 
-os_final_exp3_nfc.wake_up()
+get_opts = argparse.ArgumentParser(description='avoid duplicate wake up with args')
+
+get_opts.add_argument('--wakeup', type=bool, default=False, help="set it to boolean True when need to wake up")
+
+args = get_opts.parse_args()
+
+if args.wakeup:
+    os_final_exp3_nfc.wake_up()
+
 os_final_exp3_nfc.det_card()
