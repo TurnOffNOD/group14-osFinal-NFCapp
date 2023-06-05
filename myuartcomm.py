cf722f1a36b73b30866fd098228ce78d341b90e5
Tong Xian <tongxian22@mails.ucas.ac.cn>
2023-06-05 15:44:37 +0800

    improve read_data function

--- myuartcomm.py
+++ myuartcomm.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
 
 import serial
+import time
 
 def uart_init(portname="/dev/ttyS5", baudrate=115200, bytesize=8, parity='N', stopbits=1):
     serial_port = serial.Serial(portname, baudrate, bytesize, parity, stopbits)
     return serial_port
 
-def send_data(board, data):
-    write_count = board.write(data)
+def send_data(boardport, data):
+    write_count = boardport.write(data)
     #print(write_count)
     return
 
-def recv_data(board):
-    while board.in_waiting == 0:
-        #print("Waiting for data")
+def recv_data(boardport):
+    recved_data = bytearray()
+
+    while boardport.in_waiting ==0:
         pass
-    
-    recv_data =board.read(board.in_waiting)
-    #print("Recv: ", recv_data)
-    return recv_data
+
+    while boardport.in_waiting > 0:
+        recved_data.extend(boardport.read(boardport.in_waiting))
+        time.sleep(0.025)
+
+    #print("Recv: ", recved_data.hex(' '))
+    return recved_data
+
+def close_port(boardport):
+    boardport.close()
+    return
+
+#ser = serial.Serial("/dev/ttyUSB0", 115200, 8, 'N', 1)
+#recv_data(ser)
+#ser.close()
